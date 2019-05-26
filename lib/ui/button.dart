9ca79b1b50a24e506b7f97eecdee62111620f26c
warin001234567 <60070089@kmitl.ac.th>
2019-05-26 23:37:12 +0700

    add width

--- lib/ui/button.dart
+++ lib/ui/button.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  CustomButton({@required this.onPressed, this.text, this.height});
+  CustomButton({@required this.onPressed, this.text, this.height, this.width});
   final GestureTapCallback onPressed;
   final String text;
-  final double height;
+  final double height, width;
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       height: height,
+      width: width,
       child:RawMaterialButton(
         fillColor: Color.fromRGBO(125, 145, 193, 0.8),
 
